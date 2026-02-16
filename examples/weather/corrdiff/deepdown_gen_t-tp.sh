01ed4a2ac79e19118dc7ae332090b43adac1950c
Maxim Samarin <samarinm17@gmail.com>
2026-02-16 11:23:57 +0100

    feat: scripts for sampling from trained ERA5 -> MCH model

---
+++ examples/weather/corrdiff/deepdown_gen_t-tp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set to fail on error
+set -e
+
+#INPUT_TYPE="eobs"
+#INPUT_TYPE="coarse"
+INPUT_TYPE="era5"
+
+CONFIG="config_generate_"$INPUT_TYPE"_mch_t-tp.yaml"
+DATA_PATH="/mydata/speed2zero/shared/DeepDown/"
+STATS_PATH="/myhome/physicsnemo/corrdiff/datasets/stats/stats_"$INPUT_TYPE"_mch.json"
+TMP_PATH="/mydata/speed2zero/shared/DeepDown/tmp/target_pickle/"
+#PERIOD="train-period"
+
+echo "PREDID: $PREDID"
+echo "PERIOD: $PERIOD"
+
+#REGCKPT="/mydata/speed2zero/shared/DeepDown/physicsnemo/corrdiff/outputs/"$INPUT_TYPE"_mch_regression_t_tp/checkpoints_regression/UNet.0.3290000.mdlus"
+#DIFFCKPT="/mydata/speed2zero/shared/DeepDown/physicsnemo/corrdiff/outputs/"$INPUT_TYPE"_mch_diffusion_t_tp/checkpoints_diffusion/EDMPrecondSuperResolution.0.1740000.mdlus"
+REGCKPT="/mydata/speed2zero/shared/DeepDown/physicsnemo/corrdiff/outputs/"$INPUT_TYPE"_mch_regression_t_tp/checkpoints_regression/UNet.0.770000.mdlus"
+DIFFCKPT="/mydata/speed2zero/shared/DeepDown/physicsnemo/corrdiff/outputs/"$INPUT_TYPE"_mch_diffusion_t_tp/checkpoints_diffusion/EDMPrecondSuperResolution.0.3950000.mdlus"
+PRED="/mydata/speed2zero/shared/DeepDown/physicsnemo/corrdiff/predictions/corrdiff_pred_id-"$PREDID"_sample-10_"$PERIOD"_$(date '+%d%m%y_%H%M%S').nc"
+
+
+HYDRA_FULL_ERROR=1
+
+/usr/bin/python /myhome/physicsnemo/corrdiff/generate.py --config-name=$CONFIG ++dataset.data_path=$DATA_PATH ++dataset.stats_path=$STATS_PATH ++dataset.tmp_path=$TMP_PATH ++dataset.period=$PERIOD ++generation.io.reg_ckpt_filename=$REGCKPT ++generation.io.res_ckpt_filename=$DIFFCKPT ++generation.io.output_filename=$PRED
